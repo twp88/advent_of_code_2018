0d9f6413d32ff1e9514c16d4c6407017812ab02e
Tom <tom.w.page@gmail.com>
2018-12-06 18:56:51 +0100

    Add spec for day_one_part_two

---
+++ spec/day_one/part_two/day_one_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe DayOnePartTwo do
+  subject { DayOnePartTwo }
+
+  let(:file_zero) { 'spec/extra_files/day_one/zero.txt' }
+  let(:first_repeat_frequency) { 0 }
+
+  context 'when called' do
+    it 'returns the correct first_repeat_frequency' do
+      expect(subject.call(file)).to eq first_repeat_frequency
+    end
+  end
+end
