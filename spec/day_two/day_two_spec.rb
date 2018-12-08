45c912ddc18d813b9bdff6be497f4e9ea17189ab
Tom <tom.w.page@gmail.com>
2018-12-08 12:41:57 +0100

    Add first test for day_two

---
+++ spec/day_two/day_two_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe DayTwo do
+  subject { DayTwo }
+  let(:file) { 'spec/extra_files/day_two/twelve.txt' }
+  let(:twelve) { 12 }
+
+  context '#call' do
+    it 'returns the number 12' do
+      expect(subject.call(file)).to eq twelve
+    end
+  end
+end
