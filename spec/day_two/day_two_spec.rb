7857a8e912b1c57f0f7328cc0c9dfe414c32ccee
Tom <tom.w.page@gmail.com>
2018-12-09 13:44:13 +0100

    Add and Refactor day_two_spec

--- spec/day_two/day_two_spec.rb
+++ spec/day_two/day_two_spec.rb
@@ -2,12 +2,26 @@ require 'spec_helper'
 
 describe DayTwo do
   subject { DayTwo.new }
-  let(:file) { 'spec/extra_files/day_two/twelve.txt' }
-  let(:twelve) { 12 }
 
-  context '#call' do
-    it 'returns the number 12' do
-      expect(subject.call(file)).to eq twelve
+  describe '#call' do
+    shared_examples 'returns the correct checksum' do
+      it 'returns the number 12' do
+        expect(subject.call(file)).to eq check_sum
+      end
+    end
+
+    context 'returns checksum 12' do
+      let(:file) { 'spec/extra_files/day_two/twelve.txt' }
+      let(:check_sum) { 12 }
+
+      it_behaves_like 'returns the correct checksum'
+    end
+
+    context 'returns checksum 20' do
+      let(:file) { 'spec/extra_files/day_two/twenty.txt' }
+      let(:check_sum) { 20 }
+
+      it_behaves_like 'returns the correct checksum'
     end
   end
 end
